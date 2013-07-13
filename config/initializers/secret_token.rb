80f9ee75c77bd7689887e50f4aa387645ffba206
Hidenori Miyoshi <miyohide@gmail.com>
2013-07-14 01:11:27 +0900

    initial commit. rails new & rspec:install, sorcery:install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CancanSample::Application.config.secret_token = 'b6c6c7785f06366981d924a5a4d6675f49c70e011493c739cb7b428243cc9cb6135ce59808cad5bb054b49304ab809ed2afe9cbf4afcc03134f1c6b3ac8b00a0'
