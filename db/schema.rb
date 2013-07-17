618a99fcf1d02bfc198ba118ecf2b11e1207c60a
Hidenori Miyoshi <miyohide@gmail.com>
2013-07-17 23:14:50 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713160953) do
+ActiveRecord::Schema.define(:version => 20130717141142) do
+
+  create_table "diaries", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username",         :null => false
