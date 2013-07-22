d3fa0dd93a0ea92c7971a992d67e573d333b6fc7
Hidenori Miyoshi <miyohide@gmail.com>
2013-07-22 23:31:25 +0900

    rails g migration add_roles_mask_to_users roles_mask:integer\nMany roles per user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130717141142) do
+ActiveRecord::Schema.define(:version => 20130722143023) do
 
   create_table "diaries", :force => true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130717141142) do
     t.string   "salt"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.integer  "roles_mask"
   end
 
 end
